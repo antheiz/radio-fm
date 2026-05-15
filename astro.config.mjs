c764b09d31889d29104bebcd56f0228ada0fa4dd
antheiz <45439030+antheiz@users.noreply.github.com>
2026-05-15 09:19:21 +0900

    chore: update site URL and set base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://antheiz.github.io/radio/',
+  site: "https://antheiz.github.io",
+  base: "/radio",
   integrations: [tailwind(), svelte()],
 });
